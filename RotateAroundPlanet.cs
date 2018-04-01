360f2b1e0df0abcaeb5d96e61298cbf4f1944e8e
Zhangyzh36 <771235191@qq.com>
2018-04-01 23:30:44 +0800

    Add files via upload

---
+++ RotateAroundPlanet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPlanet : MonoBehaviour {
+
+	public Transform origin;
+	public float speed;
+	float axisX, axisY;
+	// Use this for initialization
+	void Start () {
+		axisX = Random.Range (-3,3);
+		axisY = Random.Range (15, 20);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 axis = new Vector3(axisX, axisY, 0);  
+		this.transform.RotateAround(origin.position, axis, speed * Time.deltaTime);  
+	}
+}
---
+++ ShadeOfEarth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadeOfEarth : MonoBehaviour {
+
+	public Transform trans;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = trans.position;	
+	}
+}
